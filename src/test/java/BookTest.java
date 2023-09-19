fa9ee829ed8ef120666f8ebcfbe852c5fca1931f
John <ashton.basile@gmail.com>
2023-09-19 20:24:34 +0100

    I wrote the tests for the Book class as part of TDD.

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "Fantasy");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Harry Potter and the Philosopher's Stone", book.getName());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("J.K. Rowling", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("Fantasy", book.getGenre());
+    }
+
+}
